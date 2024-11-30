43a3935e5723943ba9c22d8cef3a4a71e56044ff
YashPatel50 <patelshay8@gmail.com>
2024-11-30 17:22:53 -0500

    Attempt at making a get request

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rooms/', views.RoomView.as_view())
+    path('rooms/', views.RoomView.as_view()),
+    path('', views.TestView.as_view())
 ]
--- recipe_app/views.py
+++ recipe_app/views.py
@@ -3,10 +3,21 @@ from rest_framework import generics
 from .serializers import RoomSerializer
 from recipe_app.models import Room
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+# Create your views here.
 class RoomView(generics.ListAPIView):
     #What to return
     queryset = Room.objects.all()
     #How to convert format 
     serializer_class = RoomSerializer
+
+class TestView(APIView):
+    def get(self, request, format=None):
+        return Response({'Testing connection'}, status=status.HTTP_200_OK)
+
+
+
